c127d35bec7e34403ec8deab7ea5bf1cee7b1f8a
DuskMane <emrhn.a348@gmail.com>
2022-10-26 00:40:37 +0300

    todo app beginning

---
+++ todo-app/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+  return (
+
+    <footer className='info'>
+        <p>Click to edit todo</p>
+        <p>
+            Created by <a href='www.google.com'>Tech Career</a>
+        </p>
+    </footer>
+  )
+}
+
+export default Footer
